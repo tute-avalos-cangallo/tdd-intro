9d57d39c2a3ab8d0a6f41490e2e49838575c4f62
tute-avalos-cangallo <matias.avalos@escuelacangallo.edu.ar>
2024-04-03 14:16:40 -0300

    Se añaden tests de bases de datos

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from constantes import DB_HOSTNAME,DB_DATABASE,DB_PASSWORD,DB_USERNAME
-#import constantes
+from typing import List
 import mysql.connector as db
+#import constantes
+from constantes import DB_HOSTNAME,DB_DATABASE,DB_PASSWORD,DB_USERNAME
 
-def abrir_conexion():
+
+def abrir_conexion() -> db.pooling.PooledMySQLConnection | db.MySQLConnection:
     return db.connect(host=DB_HOSTNAME,
                       user=DB_USERNAME,
                       password=DB_PASSWORD,
                       database=DB_DATABASE)
 
+def consulta_generica(conn : db.MySQLConnection, consulta : str) -> List[db.connection.RowType]:
+    cursor = conn.cursor(buffered=True)
+    cursor.execute(consulta)
+    return cursor.fetchall()
+
 if __name__ == "__main__":
-    pass
+    conn = abrir_conexion()
+
+    resultado = consulta_generica(conn, "SHOW TABLES")
+    for row in resultado:
+        print(row[0]) # <- se muestra la primera columna de las filas resultantes
+
+    conn.close()
--- test_main.py
+++
@@ -1,9 +0,0 @@
-import main
-
-def test_abrir_conexion_existe():
-    at_main = dir(main)
-    assert "abrir_conexion" in at_main
-
-def test_abrir_conexion():
-    conn = main.abrir_conexion()
-    assert conn
